51103cf5a0b31008b5a83c1385f9ffee621d7e80
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2021-01-15 01:48:17 +0800

    fix router for login and adding files for manager

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -8,21 +9,26 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from "@angular/material/card";
 import { RouterModule, Routes } from "@angular/router";
+import { ManagerHomeComponent} from "./TestCentreManager/manager-home.component";
 
 import { LoginComponent } from "./login/login.component";
 
 const appRoutes:Routes = [
   {path: 'login', component: LoginComponent},
+  {path: 'manager-home', component: ManagerHomeComponent}
 ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ManagerHomeComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
+    FormsModule,
     MatToolbarModule,
     MatButtonModule,
     MatCardModule,
