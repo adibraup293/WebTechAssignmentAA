e1886824bf2b2cea008954dec76a86b10b9bcb7e
danieltoh16 <40682209+danieltoh16@users.noreply.github.com>
2021-01-15 17:05:20 +0800

    Added design for patient and tester home pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,14 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatButtonModule} from '@angular/material/button';
 import { MatCardModule } from "@angular/material/card";
 import { RouterModule, Routes } from "@angular/router";
-import { ManagerHomeComponent} from "./TestCentreManager/manager-home.component";
 
 import { LoginComponent } from "./login/login.component";
+import { ManagerHomeComponent} from "./TestCentreManager/manager-home.component";
+//add routing for other test center manager pages here
+import { TesterHomeComponent} from "./Tester/tester-home.component";
+//add routing for other tester pages here
+import { PatientHomeComponent} from "./Patient/patient-home.component";
+//add routing for other patient pages here
 
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faSquare, faCheckSquare, faGlobe } from '@fortawesome/free-solid-svg-icons';
@@ -19,14 +24,24 @@ import { faSquare as farSquare, faCheckSquare as farCheckSquare } from '@fortawe
 
 const appRoutes:Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'manager-home', component: ManagerHomeComponent}
+  {path: 'manager-home', component: ManagerHomeComponent},
+  //add routing for other test center manager pages here
+  {path: 'tester-home', component: TesterHomeComponent},
+  //add routing for other tester pages here
+  {path: 'patient-home', component: PatientHomeComponent}
+  //add routing for other patient pages here
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ManagerHomeComponent
+    ManagerHomeComponent,
+    //add routing for other test center manager pages here
+    TesterHomeComponent,
+    //add routing for other tester pages here
+    PatientHomeComponent
+    //add routing for other patient pages here
   ],
   imports: [
     BrowserModule,
