2ea5eb45a08643bdd614fdfbbd1236e7241de9df
adibraup293 <36180086+adibraup293@users.noreply.github.com>
2021-02-07 08:24:06 +0800

    updated back-end

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,10 @@ const TestKit = require('./models/testKit');
 const TestCentre = require('./models/testCentre');
 const TestCentreOfficer = require('./models/testCentreOfficer');
 const mongoose = require("mongoose");
-//const bcrypt = require ("bcrypt");
-// const User = require("./models/user");
-//const jwt = require('jsonwebtoken');
-// const checkAuth = require('./middleware/check-auth');
+const bcrypt = require ("bcrypt");
+const User = require("./models/user");
+const jwt = require('jsonwebtoken');
+const checkAuth = require('./middleware/check-auth');
 
 //Add one route
 //Use express function and save as an app constant
@@ -112,54 +112,89 @@ app.get("/api/testcentres",(req, res, next)=>{
   })
 });
 
-// app.get('/api/testkits', (req,res,next) => {
-//   TestKit.find().then(documents => {
-//     res.status(200).json({
-//       message: 'Testkit fetched successfully',
-//       testkits: documents
-//     });
-//   });
-// });
 
 //-----------------------------------------Test Centre Officer------------------------------------------------
 app.post("/api/testcentreofficers", (req, res, next) => {
-  const testCentreOfficer = new TestCentreOfficer({
-    testCentreOfficerName: req.body.testCentreOfficerName,
-    testCentreOfficerUsername: req.body.testCentreOfficerUsername,
-    testCentreOfficerPassword: req.body.testCentreOfficerPassword,
-    testCentreOfficerPosition: "Tester",
-    testCentreId: req.body.testCentreId
-  })
-
-  testCentreOfficer.save().then(createdTestCentreOfficer => {
-    console.log(testCentreOfficer)
-    res.status(200).json({
-      message: 'Test Centre Officer added successfully',
-      testCentreOfficerId: createdTestCentreOfficer._id
+  bcrypt.hash(req.body.testCentreOfficerPassword, 10)
+  .then(hash => {
+    const testCentreOfficer = new TestCentreOfficer({
+      testCentreOfficerName: req.body.testCentreOfficerName,
+      testCentreOfficerUsername: req.body.testCentreOfficerUsername,
+      testCentreOfficerPassword: hash,
+      testCentreOfficerPosition: "Tester",
+      testCentreId: req.body.testCentreId
+  });
+    testCentreOfficer.save().then(createdTestCentreOfficer => {
+      console.log(testCentreOfficer)
+      res.status(200).json({
+        message: 'Test Centre Officer added successfully',
+        testCentreOfficerId: createdTestCentreOfficer._id
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error:err
+      });
     });
   });
 });
 
-  // app.post("/api/testcentres", (req, res, next) => {
-  //   const testCentre = new TestCentre({
-  //     testcentrename: req.body.testcentrename,
-  //     testcentreaddress: req.body.testcentreaddress,
-  //     testcentrecontact: req.body.testcentrecontact
-  //   })
+//-----------------------------------------User Login/Signup------------------------------------------------
+app.post('/api/user/signup', (req,res,next) => {
+  bcrypt.hash(req.body.password, 10)
+  .then(hash =>{
+    const user = new User({
+      name: req.body.name,
+      username: req.body.username,
+      password: hash
+    });
+    user.save()
+    .then(result => {
+      res.status(201).json({
+        message: 'User successfully registered',
+        result: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error:err
+      });
+    });
+  });
+});
 
-  //   testCentre.save().then(createdTestCentre => {
-  //     console.log(testCentre)
-  //     res.status(200).json({
-  //       message: 'Test Centre added successfully',
-  //       testCentreId: createdTestCentre._id
-  //     });
-  //   });
-
-  //   console.log(testCentre);
-  //   res.status(201).json({
-  //     message: 'Test Centre added successfully'
-  //   });
-  // });
-
+ app.post('/api/user/login', (req,res,next) => {
+   let fetchedUser;
+   User.findOne({username: req.body.username})
+   .then(user => {
+     if (!user){
+       return res.status(401).json({
+         message: 'Auth failed'
+       });
+     }
+     fetchedUser = user
+     return bcrypt.compare(req.body.password, user.password)
+   })
+   .then(result => {
+     if (!result){
+       return res.status(401).json({
+         message: 'Auth failed'
+       });
+     }
+     const token = jwt.sign(
+       {username: fetchedUser.username, userId: fetchedUser._id},
+       'secret_this_should_be_longer',
+       {expiresIn: '1h'}
+     );
+     res.status(200).json({
+       token: token
+     })
+   })
+   .catch (err=> {
+     return res.status(401).json({
+       message: 'Auth failed'
+     });
+   })
+ })
 
 module.exports = app;
